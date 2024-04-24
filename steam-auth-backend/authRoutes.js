f115b403d9b8c7401b3ad76bd520bb7c10124244
yifanwow <yifanovo@gmail.com>
2024-04-24 16:34:13 -0400

    save user_game in local

--- steam-auth-backend/authRoutes.js
+++ steam-auth-backend/authRoutes.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const passport = require('passport');
 const axios = require('axios'); // Import Axios for HTTP requests
+const { log } = require('console');
 
 router.get('/auth/steam', passport.authenticate('steam', { failureRedirect: '/' }));
 
@@ -25,20 +26,28 @@ router.get('/auth/steam/return',
 
       const steamID = req.user._json.steamid;
       const apiKey = process.env.STEAM_API_KEY;
-      const response = await axios.get(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${apiKey}&steamid=${steamID}`);
-      if (response.data && response.data.response) {
-        const gameCount = response.data.response.game_count;
-        console.log('Game count:', gameCount);
-      }
+
+      const { gameCount, accountValue, totalGameHours } = await fetchUserAccountData(steamID);
+
+
+      // const responseAccount = await axios.get(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${apiKey}&steamids=${steamID}`);
+      // console.log(responseAccount.data);
+      // if (responseAccount.data && responseAccount.data.response && responseAccount.data.response.players && responseAccount.data.response.players.length > 0) {
+      //   accountValue = responseAccount.data.response.players[0].wallet_balance; // Assuming wallet_balance represents the account value
+      //   console.log('Account value:', accountValue);
+      // }
+
 
       // Save user data to session by add data to the json file.
       req.session.user = {
         username: req.user.displayName,
         avatarUrl: req.user._json.avatarfull,
         date: daysDiff,
+        gameCount: gameCount,
+        totalGameHours: totalGameHours,
         //registerDate: calculateRegisterDate(req.user._json.created)
       };
-
+      fetchUserGameList(steamID, apiKey);
       const filePath = path.join(__dirname, 'public', 'data.json'); // 确保路径正确
       fs.writeFile(filePath, JSON.stringify(req.session.user), (err) => {
         if (err) {
@@ -54,5 +63,79 @@ router.get('/auth/steam/return',
 
 
   });
+
+
+async function fetchUserAccountData(steamID) {
+  const apiKey = process.env.STEAM_API_KEY;
+  let gameCount = 0;
+  let accountValue = 0;
+  let totalGameHours = 0;
+
+  try {
+    const responseGames = await axios.get(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v1`, {
+      params: {
+        key: apiKey,
+        steamid: steamID
+      }
+    });
+
+    if (responseGames.data && responseGames.data.response) {
+      // Get game count
+      gameCount = responseGames.data.response.game_count;
+      console.log('Game count:', gameCount);
+
+      // Calculate total game hours
+      if (responseGames.data && responseGames.data.response && responseGames.data.response.total_count > 0) {
+        totalGameHours = responseGames.data.response.games.reduce((acc, game) => acc + game.playtime_forever, 0);
+        totalGameHours = Math.floor(totalGameHours / 60);
+        console.log('Total game hours:', totalGameHours);
+      }
+      // Get game prices
+
+      const appId = 113020;
+      const responseGamePrices = await axios.get(`https://store.steampowered.com/api/appdetails`, {
+        params: {
+          appids: appId
+        }
+      });
+      // Add each game's price to the account value
+      if (responseGamePrices.data && responseGamePrices.data[appId] && responseGamePrices.data[appId].data && responseGamePrices.data[appId].data.price_overview && responseGamePrices.data[appId].data.price_overview.final) {
+        const gamePrice = responseGamePrices.data[appId].data.price_overview.final;
+        accountValue += gamePrice;
+        console.log('Game price:', gamePrice);
+      } else {
+        console.error('Failed to fetch game price: Invalid response data');
+      }
+
+
+      console.log('Account value:', accountValue);
+    }
+  } catch (error) {
+    console.error('Error fetching additional user data:', error);
+  }
+
+  return { gameCount, accountValue, totalGameHours };
+
+}
+
+
+async function fetchUserGameList(steamID, apiKey) {
+  try {
+    const responseGames = await axios.get(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${apiKey}&steamid=${steamID}`);
+    if (responseGames.data && responseGames.data.response) {
+      const gameIDs = responseGames.data.response.games.map(game => game.appid);
+      // 将游戏 App ID 存储到 JSON 文件中
+      const filePath = path.join(__dirname, 'public', 'user_games.json');
+      fs.writeFileSync(filePath, JSON.stringify(gameIDs));
+      console.log('User game list stored successfully.');
+    } else {
+      console.error('Failed to fetch user game list: Invalid response data');
+    }
+  } catch (error) {
+    console.error('Error fetching user game list:', error);
+  }
+}
+
+
 
 module.exports = router;
