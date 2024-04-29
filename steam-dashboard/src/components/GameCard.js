e4a477d9254725001e68623397a006a2bb358e05
yifanwow <yifanovo@gmail.com>
2024-04-29 06:46:17 -0400

    Set the maximum length of the game name to make sure the gamecard is the same size, and use react's animation effect to autoplay longer game names and auto-fill the game name to optimize the visual effect.

--- steam-dashboard/src/components/GameCard.js
+++ steam-dashboard/src/components/GameCard.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import './GameCard.css';  // 引入 CSS 文件来管理样式
+import React, { useRef, useEffect, useState } from 'react';
+import './GameCard.css'; // 确保你已经引入了CSS文件
 
 function GameCard({ game }) {
+  const textRef = useRef(null);
+  const [isLongText, setIsLongText] = useState(false);
+
+  useEffect(() => {
+    if (textRef.current.scrollWidth > textRef.current.offsetWidth) {
+      setIsLongText(true); // 如果文本宽度超出容器宽度，则启动滚动
+      // 复制文本并将其追加到末尾，例如 "ABCDEFG   ABCDEFG"
+      const clonedText = `${game.name.toUpperCase()}${'\u00A0'.repeat(10)}${game.name.toUpperCase()}`;
+      textRef.current.innerText = clonedText;
+    }
+  }, []);
+
+
+
   return (
     <div className="game-card">
       <img src={game.grid} alt={game.name} className="game-image" />
-      <div className="game-name">{game.name.toUpperCase()}</div>
+      <div className="game-name-container">
+        <div
+          ref={textRef}
+          className={`game-name ${isLongText ? 'long-text' : ''}`}
+        >
+          {game.name.toUpperCase()}
+        </div>
+      </div>
     </div>
   );
 }
--- steam-dashboard/src/components/Header.js
+++ steam-dashboard/src/components/Header.js
@@ -15,33 +15,33 @@ const headerContainerStyle = {
   justifyContent: 'space-around', // 将按钮均匀分布在容器中
   alignItems: 'center',   // 垂直居中按钮
   position: 'relative',
-  top: '5vh',
+  top: '3vh',
   left: '0',
   right: '0',
   margin: 'auto',
   width: '70%',
   height: '50px',
 
-// 高斯模糊
+  // 高斯模糊
   borderRadius: '17px',
-  
+
   zIndex: '999', // 确保在最上层
 };
 
 const buttonStyle = {
   padding: '13px 30px',   // 按钮内部间距
   background: 'none',    // 透明背景
- // border: '2px solid white', // 白色边框
+  // border: '2px solid white', // 白色边框
   borderRadius: '10px',  // 圆角
   cursor: 'pointer',     // 鼠标样式变为指针
-  color: 'white', 
-  backgroundColor: 'rgba(240, 240, 240, 0.5)', 
+  color: 'white',
+  backgroundColor: 'rgba(240, 240, 240, 0.5)',
   boxShadow: '0px 0px 11px rgba(0, 0, 0, 0.37)',
   backdropFilter: 'blur(11px)',       // 文字颜色
   fontSize: '16px',      // 字号
   fontWeight: 'bold',     // 字体加粗
   textShadow: '0px 0px 7px rgba(0, 0, 0, 0.7)'
- 
+
 };
 
 export default Header;
