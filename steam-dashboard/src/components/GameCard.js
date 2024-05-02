f87f43676d4bdb27d3bb72618d0692e5984ffb4d
gloriahu28 <rh692@cornell.edu>
2024-05-01 23:15:42 -0400

    Wrote backend, conencted backend with front end, and updated css for rating appearance and functionality

--- steam-dashboard/src/components/GameCard.js
+++ steam-dashboard/src/components/GameCard.js
@@ -2,16 +2,16 @@ import React, { useRef, useEffect, useState } from 'react';
 import './GameCard.css'; // 确保你已经引入了CSS文件
 import UpdateGameGrid from './UpdateGameGrid';
 import Rating from '@mui/material/Rating'; // Import the Rating Component
-import FavoriteIcon from '@mui/icons-material/Favorite'; 
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import { styled } from '@mui/material/styles';
 
 const StyledRating = styled(Rating)({
   '& .MuiRating-iconFilled': {
-    color: '#ff6d75',
+    color: '#ffffff',
   },
   '& .MuiRating-iconHover': {
-    color: '#ff3d47',
+    color: '#ffffff',
   },
 });
 
@@ -21,7 +21,7 @@ function GameCard({ game }) {
   const [menuVisible, setMenuVisible] = useState(false);
   const [imageUrl, setImageUrl] = useState(game.grid);
   const [ratingVisible, setRatingVisible] = useState(false);
-  const [rating, setRating] = useState(2); // State to hold the rating
+  const [rating, setRating] = useState(game.rate); // State to hold the rating
 
   const toggleMenu = () => {
     setMenuVisible(!menuVisible);
@@ -37,15 +37,15 @@ function GameCard({ game }) {
       },
       body: JSON.stringify({ appid: appId, newGridUrl })
     })
-    .then(response => response.json())
-    .then(data => {
-      alert('Grid URL updated successfully.');
-      setImageUrl(newGridUrl);  // 成功后更新图片 URL
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-      alert('Failed to update grid URL.');
-    });
+      .then(response => response.json())
+      .then(data => {
+        alert('Grid URL updated successfully.');
+        setImageUrl(newGridUrl);  // 成功后更新图片 URL
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        alert('Failed to update grid URL.');
+      });
   };
 
   const handleChangeGrid = () => {
@@ -56,6 +56,43 @@ function GameCard({ game }) {
   const toggleRating = () => {
     setRatingVisible(!ratingVisible);
   };
+
+  const handleChangeRating = (val) => {
+    console.log(val)
+    onUpdateRating(game.appid, val);
+  };
+
+  const handleDeleteRate = () => {
+    onUpdateRating(game.appid, -1)
+  }
+
+  // Update Rating front-end
+  const onUpdateRating = (appId, rate) => {
+
+    fetch(`${process.env.REACT_APP_API_BASE}/update-rate`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ appid: appId, newRate: rate })
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (rate === -1) {
+          alert('Rating deleted successfully.');
+          setRating(null);
+        } else {
+          alert('Rating updated successfully.');
+          setRating(rate);  // Enables rating succesfully
+        }
+        setRatingVisible(false)
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        alert('Failed to update grid URL.');
+      });
+  };
+
 
   useEffect(() => {
     if (textRef.current.scrollWidth > textRef.current.offsetWidth) {
@@ -74,21 +111,35 @@ function GameCard({ game }) {
         <div ref={textRef} className={`game-name ${isLongText ? 'long-text' : ''}`}>
           {game.name.toUpperCase()}
         </div>
-        
+
       </div>
       <div className="game-rate-container">
-        <StyledRating
-        name="customized-color"
-        defaultValue={2}
-        getLabelText={(value) => `${value} Heart${value !== 1 ? 's' : ''}`}
-        precision={0.5}
-        icon={<FavoriteIcon fontSize="inherit" />}
-        emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
-      />  
-        </div>
+        {ratingVisible ?
+          <StyledRating
+            name="customized-color"
+            value={rating}
+            precision={0.5}
+            icon={<FavoriteIcon fontSize="inherit" />}
+            emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+            onChange={(event, newValue) => {
+              handleChangeRating(newValue);
+            }}
+          /> : rating && <StyledRating
+            name="customized-color"
+            value={rating}
+            readOnly
+            precision={0.5}
+            icon={<FavoriteIcon fontSize="inherit" />}
+            emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+          />
+        }
+      </div>
       <div className={`options-menu ${menuVisible ? 'active' : ''}`}>
         <div className="option-item" onClick={handleChangeGrid}>Change grid post</div>
-        <div className="option-item" onClick={toggleRating}>Rating</div>
+        {rating ?
+          <div className="option-item" onClick={handleDeleteRate}>Delete</div> :
+          <div className="option-item" onClick={toggleRating}>Rating</div>
+        }
       </div>
     </div>
   );
