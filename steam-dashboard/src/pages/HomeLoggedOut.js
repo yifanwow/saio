22cfdbe39def4bcba52ccbd2b0fe20f12a130df6
yifanwow <yifanovo@gmail.com>
2024-05-13 03:40:56 -0400

    add guest login button

--- steam-dashboard/src/pages/HomeLoggedOut.js
+++ steam-dashboard/src/pages/HomeLoggedOut.js
@@ -21,7 +21,7 @@ const leftContainerStyle = {
 
 const textGroupStyle = {
   textAlign: 'left', // Ensure text is aligned to the left
-  marginBottom: '2.7rem', // Provide some spacing between the text and the form
+  marginBottom: '1.7rem', // Provide some spacing between the text and the form
 };
 const titleGroupStyle = {
   fontSize: '3.5rem',
@@ -36,39 +36,27 @@ const AIOStyle = {
   color: '#ffffff',
 };
 
-const inputButtonContainerStyle = {
+const buttonContainerStyle = {
   display: 'flex', // Use flexbox for horizontal layout
-  alignItems: 'left', // Stretch the items to fill the container height
-  background: '#121212', // Dark background for the input
-  borderRadius: '11px',
-  border: '2px solid #4a4d54',
-  overflow: 'hidden', // Prevents child elements from overflowing
-  marginBottom: '1.3rem',
-  width: '77%',
-};
-
-const inputStyle = {
-  flexGrow: 1, // Input should take all available space
-  padding: '10px 15px',
-  fontSize: '1rem',
-  border: 'none', // Remove border since it's now inside the container
-  outline: 'none',
-  color: '#fff', // Text color for the input
-  backgroundColor: '#121212',
-  width: '30%',
+  flexDirection: 'column', // Align buttons vertically
+  alignItems: 'center', // Center buttons horizontally
+  marginBottom: '1.5rem',
+  width: '50%',
+  
 };
 
 const buttonStyle = {
   padding: '10px 27px',
-  border: 'none',
+  margin: '10px 0', // Add space between buttons
+  border: '2px solid #46464657',// 细细的白色描边
   backgroundColor: '#2c2242',
   color: '#fff',
-  borderRadius: '0', // Reset border-radius as the container now controls this
+  borderRadius: '11px', // Apply rounded corners
   cursor: 'pointer',
-
+  width: '100%', // Make buttons full-width of the container
 };
 
-const GitHubStyle ={
+const GitHubStyle = {
   display: 'flex',
   fontSize: '0.77em', // 字体大小为原来的 80%
   alignItems: 'center', // Aligns the icon and text vertically
@@ -78,43 +66,44 @@ const GitHubStyle ={
 };
 
 function HomeLoggedOut() {
-  // Function to handle the form submission
-  console.log('Try to login in using API Base URL:', process.env.REACT_APP_API_BASE_URL);
-  const handleSubmit = (e) => {
+  // Function to handle the form submission for normal login
+  const handleLogin = (e) => {
     e.preventDefault();
-    window.location.href = authUrl;
+    window.location.href = authUrl; // Redirect to auth URL
     console.log('LOGIN IN HomeLoggedOut-checkpoint 2');
-    // Perform your login logic here
+  };
+
+  // Function to handle the guest login
+  const handleGuestLogin = () => {
+    window.location.href = "http://saio.us-east-2.elasticbeanstalk.com/home?steamid=76561198856798776";
   };
 
   return (
     <div style={homeStyle}>
       <div style={leftContainerStyle}>
         <div>
-        <div style={textGroupStyle}>
-          <h1 style={titleGroupStyle}>
-            <span style={SteamStyle}>STEAM </span>
-            <span style={AIOStyle}>A I O</span>
-          </h1>
-          <p>All in one place</p>
-        </div>
-        <form onSubmit={handleSubmit} style={inputButtonContainerStyle}>
-          <input type="text" placeholder="Steam ID" style={inputStyle} />
-          <button type="submit" style={buttonStyle}>Login</button>
-        </form>
-        <div style={GitHubStyle}>
-        <img src="/img/ICON/GITHUB_GRAY.png" alt="GitHub Icon" style={{ width: '21px' }} />
-          <a href="https://github.com/yifanwow/wishMeLuck" target="_blank" rel="noopener noreferrer" style={{ color: '#fff', textDecoration: 'none' }}>
-            GitHub
-          </a>
-          
-        </div>
-        {/* Other content like GitHub link, version info, etc. */}
-        
-      </div>
-      <div style={{ fontSize: '0.77em', color: '#5a4d54', position: 'absolute', bottom: '10px', alignItems: 'center' }}>
-            version 0.01
+          <div style={textGroupStyle}>
+            <h1 style={titleGroupStyle}>
+              <span style={SteamStyle}>STEAM </span>
+              <span style={AIOStyle}>A I O</span>
+            </h1>
+            <p>All in one place</p>
           </div>
+          <div style={buttonContainerStyle}>
+            <button onClick={handleLogin} style={buttonStyle}>Login</button>
+            <button onClick={handleGuestLogin} style={buttonStyle}>Guest Login</button>
+          </div>
+          <div style={GitHubStyle}>
+            <img src="/img/ICON/GITHUB_GRAY.png" alt="GitHub Icon" style={{ width: '21px' }} />
+            <a href="https://github.com/yifanwow/saio" target="_blank" rel="noopener noreferrer" style={{ color: '#fff', textDecoration: 'none' }}>
+              GitHub
+            </a>
+          </div>
+          {/* Other content like GitHub link, version info, etc. */}
+        </div>
+        <div style={{ fontSize: '0.77em', color: '#5a4d54', position: 'absolute', bottom: '10px', alignItems: 'center' }}>
+          version 0.01
+        </div>
       </div>
       
       <div style={{
